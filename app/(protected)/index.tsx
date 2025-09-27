299502f2dc5929d02b62b8429a43964fa429796c
Nishant Sood <nishantsood@duck.com>
2025-09-27 17:31:00 -0400

    added parallax scroll view updated homescreen design

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -1,25 +1,76 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { sessions } from "@/utils/sessions";
+import { Image } from "expo-image";
 import { useRouter } from "expo-router";
-import { Pressable, ScrollView, Text } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
   return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      {sessions.map((session) => (
-        <Pressable
-          key={session.id}
-          style={{ borderWidth: 1, padding: 16, marginVertical: 8 }}
-          onPress={() =>
-            router.navigate({
-              pathname: "/session",
-              params: { sessionId: session.id },
-            })
-          }
-        >
-          <Text>{session.title}</Text>
-        </Pressable>
-      ))}
-    </ScrollView>
+    <ParallaxScrollView>
+      <Text style={styles.title}>Explore Sessions</Text>
+      <ScrollView
+        contentContainerStyle={{ paddingLeft: 16, gap: 6 }}
+        horizontal
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        {sessions.map((session) => (
+          <Pressable
+            key={session.id}
+            style={styles.sessionContainer}
+            onPress={() =>
+              router.navigate({
+                pathname: "/session",
+                params: { sessionId: session.id },
+              })
+            }
+          >
+            <Image
+              source={session.image}
+              style={styles.sessionImage}
+              contentFit="cover"
+              transition={1000}
+            />
+            <View
+              style={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                experimental_backgroundImage:
+                  "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5))",
+                borderRadius: 16,
+              }}
+            />
+            <Text style={styles.sessionTitle}>{session.title}</Text>
+          </Pressable>
+        ))}
+      </ScrollView>
+    </ParallaxScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: 16,
+  },
+  sessionContainer: {
+    position: "relative",
+  },
+  sessionImage: {
+    width: 250,
+    height: 140,
+    borderRadius: 16,
+    overflow: "hidden",
+  },
+  sessionTitle: {
+    position: "absolute",
+    width: "100%",
+    bottom: 16,
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+});
---
+++ components/ParallaxScrollView.tsx
@@ -0,0 +1,110 @@
+import { sessions } from "@/utils/sessions";
+import { Image } from "expo-image";
+import React, { PropsWithChildren } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Animated, {
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from "react-native-reanimated";
+import Button from "./Button";
+
+const HEADER_HEIGHT = 400;
+
+const ParallaxScrollView = ({ children }: PropsWithChildren) => {
+  const todaySession = sessions[Math.floor(Math.random() * sessions.length)];
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  const scrollOffset = useScrollViewOffset(scrollRef);
+
+  const headerAnimatedStyle = useAnimatedStyle(() => {
+    const translateY =
+      scrollOffset.value <= 0
+        ? interpolate(
+            scrollOffset.value,
+            [-HEADER_HEIGHT, 0],
+            [-HEADER_HEIGHT / 2, 0]
+          )
+        : 0;
+
+    const scale =
+      scrollOffset.value <= 0
+        ? interpolate(scrollOffset.value, [-HEADER_HEIGHT, 0], [2, 1])
+        : 1;
+
+    return {
+      transform: [{ translateY }, { scale }],
+    };
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.ScrollView
+        ref={scrollRef}
+        scrollEventThrottle={16}
+        showsVerticalScrollIndicator={false}
+      >
+        <Animated.View
+          style={[
+            { height: HEADER_HEIGHT, overflow: "hidden" },
+            headerAnimatedStyle,
+          ]}
+        >
+          <Image
+            source={todaySession.image}
+            style={{ width: "100%", height: HEADER_HEIGHT }}
+          />
+        </Animated.View>
+        <View style={styles.headerContainer}>
+          <View style={styles.headerContent}>
+            <View style={{ flex: 5 }} />
+            <Text style={styles.headerSubtitle}>Featured Session</Text>
+            <Text style={styles.headerTitle}>{todaySession.title}</Text>
+            <Text style={styles.headerDescription}>
+              {todaySession.description}
+            </Text>
+            <Button>Start Session</Button>
+            <View style={{ flex: 1 }} />
+          </View>
+        </View>
+        {children}
+      </Animated.ScrollView>
+    </View>
+  );
+};
+
+export default ParallaxScrollView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerSubtitle: {
+    fontSize: 16,
+    color: "white",
+    opacity: 0.5,
+    fontWeight: "bold",
+  },
+  headerTitle: {
+    fontSize: 48,
+    color: "white",
+    fontWeight: "bold",
+  },
+  headerDescription: {
+    fontSize: 16,
+    color: "white",
+  },
+  headerContainer: {
+    position: "absolute",
+    width: "100%",
+    height: HEADER_HEIGHT,
+    experimental_backgroundImage:
+      "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5))",
+  },
+  headerContent: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8,
+  },
+});
