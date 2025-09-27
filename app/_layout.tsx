b5b97226098f7b9ccbb989d7f8ff60d5149a7a80
Nishant Sood <nishantsood@duck.com>
2025-09-27 00:30:30 -0400

    added auth routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
+import { authClient } from "@/lib/auth-client";
 import { Stack } from "expo-router";
 
+function RootLayoutWithAuth() {
+  const { data, isPending } = authClient.useSession();
+
+  if (isPending) {
+    return null;
+  }
+  return (
+    <Stack>
+      <Stack.Protected guard={!isPending && data !== null}>
+        <Stack.Screen name="(protected)" />
+      </Stack.Protected>
+      <Stack.Protected guard={isPending || data == null}>
+        <Stack.Screen name="(public)" />
+      </Stack.Protected>
+    </Stack>
+  );
+}
+
 export default function RootLayout() {
-  return <Stack />;
+  return <RootLayoutWithAuth />;
 }
